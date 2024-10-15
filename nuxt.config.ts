d016d29e904a68a09c2e47fb0a9da89f25e8cff3
swhawes <shawes@fiu.edu>
2024-10-15 10:32:29 -0400

    test deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,12 +28,49 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: [
-        '/'
+        '/' // Home page
+        // '/about', // Example additional route (add more routes as necessary)
+        // '/longit/tools', // Example
+        // '/longit/tutorials' // Example
       ],
-      crawlLinks: true
+      crawlLinks: true, // Enable crawling to find more links automatically
+      ignore: [
+        '/tools/6.data-formats',
+        '/api/_content/query/JPfzfXpE83.1728957996020.json',
+        '/plotly-view',
+        '/contribute',
+        '/sandbox',
+        '/longit/about',
+        '/longit/getting-started',
+        '/tutorials/data-visualization/viz_customizing-plots-ggplot2',
+        '/longit/tutorials/data-visualization/viz_customizing-plots-ggplot2',
+        '/longit/tutorials/longitudinal-analyses-models/growth-curve-models/multivariate_latent_growth_curve_models_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/mixed-effects-and-multilevel/hierarchical_linear_modeling_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/growth-curve-models/piecewise_growth_models_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/growth-curve-models/multilevel_growth_models_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/crosslagged-and-panel-models/autoregressive_cross_lagged_models_tutorial',
+        '/longit/abcd-study/resources',
+        '/longit/abcd-study/traditional-linear-models',
+        '/longit/tutorials/longitudinal-analyses-models/time-to-event-and-survival-models/survival_analysis_time_dependent_covariates_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/advanced-models/generalized_estimating_equations_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/mixture-models/growth_mixture_models_tutorial',
+        '/longit/tutorials/longitudinal-analyses-models/mixed-effects-and-multilevel/linear-mixedeffects-models',
+        '/longit/tools/programming-languages/r/rtestfile1',
+        '/longit/tools/programming-languages/r/rtestfile2',
+        '/longit/tutorials/longitudinal-analyses-models/mixture-models/latent_class_growth_analysis_autorial',
+        '/longit/tutorials/special-case-tutorials/advanced_bootstrapping-and-resampling',
+        '/longit/tutorials/exploratory-data-analysis/exploratory_diagnostic-plots',
+        '/longit/tutorials/data-wrangling/cleaning-and-transforming-data',
+        '/longit/tutorials/longitudinal-analyses-models/advanced-models/latent-curve-model-with-structured-residuals',
+        '/longit/tutorials/longitudinal-analyses-models/advanced-models/random_intercept_cross_lagged_panel_model_tutorial',
+        '/longit/abcd-study/traditional-nonlinear-models/examples_signedranktest',
+        '/longit/tutorials/exploratory-data-analysis/exploratory_model-evaluation'
+        // Add more routes to ignore as needed
+      ]
     }
   },
 
+  // other configuration...
   routeRules: {
     '/api/search.json': { prerender: true }
   },
