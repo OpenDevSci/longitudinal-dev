4b8cc835308c634a197dd009f108ffda07cf66d9
swhawes <shawes@fiu.edu>
2024-10-24 13:47:00 -0400

    testing icon display

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,6 @@ export default defineNuxtConfig({
   content: {
     documentDriven: true, // Ensuring document-driven mode is correctly enabled
     highlight: {
-      preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'md', 'yaml', 'r', 'mdc'],
       langs: ['json', 'js', 'ts', 'html', 'css', 'vue', 'shell', 'md', 'yaml', 'r', 'mdc']
     }
   },
